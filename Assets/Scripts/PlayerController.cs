0afebe7182710bd7dad6a058d6554f22031ae608
EivindK <ekvissel@gmail.com>
2015-10-22 17:38:50 +0200

    Level

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	// public vars
+	public float mouseSensitivityX = 1;
+	public float mouseSensitivityY = 1;
+	public float walkSpeed = 6;
+	public float runSpeed = 10;
+	
+	// System vars
+	Vector3 moveAmount;
+	Vector3 smoothMoveVelocity;
+	float verticalLookRotation;
+	Transform cameraTransform;
+	Rigidbody rigidbodyP;
+	
+	
+	void Awake() {
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+		cameraTransform = Camera.main.transform;
+		rigidbodyP = GetComponent<Rigidbody> ();
+	}
+	
+	void Update() {
+		
+		// Look rotation:
+		transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * mouseSensitivityX);
+		verticalLookRotation += Input.GetAxis("Mouse Y") * mouseSensitivityY;
+		verticalLookRotation = Mathf.Clamp(verticalLookRotation,-60,60);
+		cameraTransform.localEulerAngles = Vector3.left * verticalLookRotation;
+		
+		// Calculate movement:
+		float inputX = Input.GetAxisRaw("Horizontal");
+		float inputY = Input.GetAxisRaw("Vertical");
+		
+		Vector3 moveDir = new Vector3(inputX,0, inputY).normalized;
+		
+		Vector3 targetMoveAmount;
+		// Check if Running
+		if (Input.GetKey("left shift")) {
+			targetMoveAmount = moveDir * runSpeed;
+		} else {
+			targetMoveAmount = moveDir * walkSpeed;
+		}
+		
+		moveAmount = Vector3.SmoothDamp(moveAmount,targetMoveAmount,ref smoothMoveVelocity,.15f);
+		
+	}
+	
+	void FixedUpdate() {
+		// Apply movement to rigidbody
+		Vector3 localMove = transform.TransformDirection(moveAmount) * Time.fixedDeltaTime;
+		rigidbodyP.MovePosition(rigidbodyP.position + localMove);
+	}
+}
