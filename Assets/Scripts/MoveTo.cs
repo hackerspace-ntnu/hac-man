6536b4469da831cfd607a7ef23f8d708e97d13ec
unknown <oystein.sigholt@gmail.com>
2015-10-28 18:37:54 +0100

    ai

---
+++ Assets/Scripts/MoveTo.cs
@@ -0,0 +1,13 @@
+﻿    // MoveTo.cs
+    using UnityEngine;
+    using System.Collections;
+    
+    public class MoveTo : MonoBehaviour {
+       
+       public Transform goal;
+       
+       void Start () {
+          NavMeshAgent agent = GetComponent<NavMeshAgent>();
+          agent.destination = goal.position; 
+       }
+    }
