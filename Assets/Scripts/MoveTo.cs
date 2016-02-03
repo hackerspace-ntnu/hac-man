4d5ff355723ee406a4f63c86f33254cbea8f8705
EivindK <ekvissel@gmail.com>
2016-02-03 18:04:56 +0100

    Merge

--- Assets/Scripts/MoveTo.cs
+++ Assets/Scripts/MoveTo.cs
@@ -1,25 +1,18 @@
-﻿    // MoveTo.cs
-    using UnityEngine;
-    using System.Collections;
+﻿// MoveTo.cs
+using UnityEngine;
+using System.Collections;
     
-    public class MoveTo : MonoBehaviour {
+public class MoveTo : MonoBehaviour {
        
-       public Transform goal;
+   public Transform goal;
        
-		void Start () {
-          	NavMeshAgent agent = GetComponent<NavMeshAgent>();
-          	agent.destination = goal.position;
-			agent.autoBraking = false;
-       	}
-
-<<<<<<< HEAD
+	void Start () {
+   		NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+		agent.autoBraking = false;
+    }
 	void Update() {
 	  	NavMeshAgent agent = GetComponent<NavMeshAgent>();
       	agent.destination = goal.position; 
-=======
-		void Update() {
-	  		NavMeshAgent agent = GetComponent<NavMeshAgent>();
-          	agent.destination = goal.position; 
->>>>>>> efaab94f0d6a0bc9b8a15661bdc0a40b4913edd1
-		}
-    }
+   	}
+}
