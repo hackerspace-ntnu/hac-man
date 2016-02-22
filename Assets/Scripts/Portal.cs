1d2f5405bd636e9f34825f9385fcaaf2ee1ab140
EivindK <ekvissel@gmail.com>
2016-02-22 23:32:13 +0100

    Tuned lighting and added portals

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Portal : MonoBehaviour {
+
+	public Transform targetLocation;
+
+	bool recentlyTeleported = false;
+	bool teleportedFromThisPortal = false;
+
+	public void OnTriggerEnter(Collider col) {
+
+		print ("Player entered portal: " + transform.name);
+
+		if (col.transform.tag == "Player") {
+			if (!recentlyTeleported) {
+				col.transform.position = targetLocation.position;
+				col.transform.rotation = targetLocation.rotation;
+				recentlyTeleported = true;
+				teleportedFromThisPortal = true;
+			}
+		}
+	}
+
+	public void OnTriggerExit(Collider col) {
+		if (!teleportedFromThisPortal) {
+			recentlyTeleported = false;
+		} else {
+			teleportedFromThisPortal = false;
+		}
+
+	}
+
+
+}
