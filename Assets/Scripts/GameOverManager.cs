92d65540c7f85d6e5436139ba5192c61f3124a5d
EivindK <ekvissel@gmail.com>
2016-11-19 01:59:17 +0100

    Finishing touches

---
+++ Assets/Scripts/GameOverManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverManager : MonoBehaviour {
+
+
+	public GameObject floor;
+	private GameObject[] ghosts;
+
+	// Use this for initialization
+	void Start () {
+		ghosts = GameObject.FindGameObjectsWithTag("Enemy1");
+		StartCoroutine(DelayedGameOver());
+	}
+
+	IEnumerator DelayedGameOver() { 
+		yield return new WaitForSeconds(3);
+		foreach (GameObject ghost in ghosts) {
+			Animator ghostAnimator = ghost.GetComponentInChildren<Animator>();
+			ghostAnimator.SetBool("Chasing", true);
+		}
+		yield return new WaitForSeconds(2);
+		Destroy(this.gameObject);
+
+
+	}
+}
